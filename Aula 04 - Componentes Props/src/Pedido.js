d35f6fca300e730644d2ff89b9da24cb6dc680d8
matheusgalbiati <79460905+matheusgalbiati@users.noreply.github.com>
2022-09-03 12:07:01 -0300

    Componentes usando classe

--- Aula 04 - Componentes Props/src/Pedido.js
+++ Aula 04 - Componentes Props/src/Pedido.js
@@ -1,20 +1,36 @@
 import React from 'react'
 
-const Pedido = (props) => {
+// const Pedido = (props) => {
+//     return (
+//         <div className="card">
+//             {/* cabeçalho do cartão */}
+//             <div className="card-header text-muted">{props.data}</div>
+//             {/* corpo do cartão */}
+//             <div className="card-body d-flex">
+//                 <div className="d-flex align-items-center">
+//                     <i className={props.icone}></i>
+//                 </div>
+//                 {/* flex-grow 1: tomar espaço remanescente */}
+//                 <div className="flex-grow-1 ms-2 border">
+//                     <h4 className="text-center">{props.titulo}</h4>
+//                     <p className="text-center">{props.descricao}</p>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Pedido;
+
+const Pedido = ({icone, titulo, descricao}) => {
     return (
-        <div className="card">
-            {/* cabeçalho do cartão */}
-            <div className="card-header text-muted">{props.data}</div>
-            {/* corpo do cartão */}
-            <div className="card-body d-flex">
-                <div className="d-flex align-items-center">
-                    <i className={props.icone}></i>
-                </div>
-                {/* flex-grow 1: tomar espaço remanescente */}
-                <div className="flex-grow-1 ms-2 border">
-                    <h4 className="text-center">{props.titulo}</h4>
-                    <p className="text-center">{props.descricao}</p>
-                </div>
+        <div className="d-flex">
+            <div className="d-flex align-items-center">
+                <i className={`${icone}`}></i>
+            </div>
+            <div className="flex-grow-1 ms-2 border">
+                <h4 className="text-center">{titulo}</h4>
+                <p className="text-center">{descricao}</p>
             </div>
         </div>
     )
