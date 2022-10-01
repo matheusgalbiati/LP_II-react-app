f30a218c164150358a63e388987b065af6034ed2
matheusgalbiati <79460905+matheusgalbiati@users.noreply.github.com>
2022-10-01 09:51:23 -0300

    Aula 07 clonada do Bonato

---
+++ Aula 07 - Forms, Entrada de dados, Eventos, API/src/components/App.js
@@ -0,0 +1,59 @@
+import React  from 'react'
+import Busca from './Busca'
+import env from 'react-dotenv'
+import { createClient } from 'pexels'
+import ListaImagens from './ListaImagens'
+import PexelsLogo from './Pexels'
+import pexelsClient from '../utils/pexelsClient'
+
+export default class App extends React.Component{
+    pexelsClient = null
+    state = {pics: []}
+
+    componentDidMount(){
+        this.pexelsClient = createClient(env.PEXELS_KEY)
+    }
+
+    // onBuscaRealizada = (termo) => {
+    //     this.pexelsClient.photos.search({
+    //         query: termo
+    //     })
+    //     .then(pics => this.setState({pics: pics.photos}))
+    // }
+
+    onBuscaRealizada = (termo) => {
+        pexelsClient.get('/search', {
+            params: {query: termo}
+        })
+        .then(result => {
+            console.log(result)
+            //data é um atributo definido pela axios
+            //o conteúdo da resposta vem associado a esta chave
+            this.setState({pics: result.data.photos})
+        })
+    }
+
+    render(){
+        //console.log(env.PEXELS_KEY)
+        //console.log(window.env.PEXELS_KEY)
+        return(
+            <div className='grid justify-content-center m-auto w-9 border-round border-1 border-400'>
+                <div className='col-12'>
+                    <PexelsLogo/ >
+                </div>
+                <div className='col-12'>
+                    <h1 className="text-center">Exibir uma lista de...</h1>
+                </div>
+                <div className='col-12'>
+                    <Busca onBuscaRealizada={this.onBuscaRealizada} />
+                </div>
+                <div className='col-12'>
+                    <div className='grid'>
+                        <ListaImagens imgStyle={'col-12 md:col-6 lg:col-4 xl:col-3'} 
+                            pics={this.state.pics}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ Aula 07 - Forms, Entrada de dados, Eventos, API/src/components/ListaImagens.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Imagem from "./Imagem"
+
+const ListaImagens = ({pics, imgStyle}) => {
+    return (
+        pics.map((pic,key) => 
+            <Imagem
+                imgStyle={imgStyle}
+                pic={pic.src.small}
+                key={key}
+            />
+    ))
+}
+
+export default ListaImagens
